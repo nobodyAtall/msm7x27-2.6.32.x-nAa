8f84dbe0b9b17831b23ef85f53e64fb83738b1db
Ranjhith Kalisamy <ranjhith@codeaurora.org>
2012-02-01 19:31:23 +0530

    msm: kgsl: disable irq when adreno device is stopped
    When restoring to slumber from nap, the irq is kept enabled causing a
    power drain. This change ensures that adreno device behaves similar to
    z180 device.
    
    Change-Id: I7dd9b8f21ae341c978204b6dbb0162d606168b77
    CRs-Fixed: 331847
    Signed-off-by: Ranjhith Kalisamy <ranjhith@codeaurora.org>
    Signed-off-by: nobodyAtall <tsogkas@ceid.upatras.gr>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -610,6 +610,7 @@ static int adreno_stop(struct kgsl_device *device)
 	kgsl_mmu_stop(device);
 
 	device->ftbl->irqctrl(device, 0);
+	kgsl_pwrctrl_irq(device, KGSL_PWRFLAGS_OFF);
 
 	/* Power down the device */
 	kgsl_pwrctrl_disable(device);
