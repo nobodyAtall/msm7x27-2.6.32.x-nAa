8c264318d3138af96329e7013e35b951485a0ce3
Blechd0se <alex.christ@hotmail.de>
2012-12-29 19:50:14 +0100

    writeback: remove unused nonblocking and congestion checks
    Signed-off-by: nobodyAtall <tsogkas@ceid.upatras.gr>

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -822,7 +822,6 @@ int write_cache_pages(struct address_space *mapping,
 		      struct writeback_control *wbc, writepage_t writepage,
 		      void *data)
 {
-	struct backing_dev_info *bdi = mapping->backing_dev_info;
 	int ret = 0;
 	int done = 0;
 	struct pagevec pvec;
@@ -834,11 +833,6 @@ int write_cache_pages(struct address_space *mapping,
 	int cycled;
 	int range_whole = 0;
 	long nr_to_write = wbc->nr_to_write;
-
-	if (wbc->nonblocking && bdi_write_congested(bdi)) {
-		wbc->encountered_congestion = 1;
-		return 0;
-	}
 
 	pagevec_init(&pvec, 0);
 	if (wbc->range_cyclic) {
@@ -957,12 +951,6 @@ continue_unlock:
 					done = 1;
 					break;
 				}
-			}
-
-			if (wbc->nonblocking && bdi_write_congested(bdi)) {
-				wbc->encountered_congestion = 1;
-				done = 1;
-				break;
 			}
 		}
 		pagevec_release(&pvec);
